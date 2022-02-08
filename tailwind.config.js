cd31f18bc1e3c6b921df214ad2de42dd99a3f92a
alk <konovalov.phone@gmail.com>
2022-02-08 19:38:26 +0300

    initial

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        heroBlue: {
+          50:'#50C4ED',
+          100: '#387ADF'
+        }
+      }
+    },
+  },
+  plugins: [],
+}
